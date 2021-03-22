1fd6d8667db4f058033ed05379c6a0e36f867c4f
Jayram Moorkanikara Nageswaran <jayram.nageswaran@braincorp.com>
2021-03-22 02:43:24 -0700

    first commit to repo

---
+++ src/fourier.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+%matplotlib inline
+
+# Read in the images
+image_stripes = cv2.imread('images/stripes.jpg')
+# Change color to RGB (from BGR)
+image_stripes = cv2.cvtColor(image_stripes, cv2.COLOR_BGR2RGB)
+
+# Read in the images
+image_solid = cv2.imread('images/pink_solid.jpg')
+# Change color to RGB (from BGR)
+image_solid = cv2.cvtColor(image_solid, cv2.COLOR_BGR2RGB)
+
+# Display the images
+f, (ax1,ax2) = plt.subplots(1, 2, figsize=(10,5))
+
+ax1.imshow(image_stripes)
+ax2.imshow(image_solid)
+# convert to grayscale to focus on the intensity patterns in the image
+gray_stripes = cv2.cvtColor(image_stripes, cv2.COLOR_RGB2GRAY)
+gray_solid = cv2.cvtColor(image_solid, cv2.COLOR_RGB2GRAY)
+
+# normalize the image color values from a range of [0,255] to [0,1] for further processing
+norm_stripes = gray_stripes/255.0
+norm_solid = gray_solid/255.0
+
+# perform a fast fourier transform and create a scaled, frequency transform image
+def ft_image(norm_image):
+    '''This function takes in a normalized, grayscale image
+       and returns a frequency spectrum transform of that image. '''
+    f = np.fft.fft2(norm_image)
+    fshift = np.fft.fftshift(f)
+    frequency_tx = 20*np.log(np.abs(fshift))
+    
+    return frequency_tx
+
